fe91a29d1b02385f6163950ff4618f1e0f830a32
Sam Clark <sclark@bnl.gov>
2020-05-05 11:45:52 -0400

    Updated config

--- test.py
+++ test.py
@@ -1,23 +1,25 @@
 from multinet import Multirequest
 import timeit
 
-params = [('simple.test', 'shortWatchM'), 
- ('simple.test', 'ushortWatchM'), 
- ('simple.test', 'ushortWatchStringM'), 
- ('simple.test', 'stringMonitorM'), 
- ('simple.test', 'menuM'), 
- ('simple.test', 'intM'), 
- ('simple.test', 'longM'), 
- ('simple.test', 'doubleM'), 
- ('simple.test', 'intArrayM'), 
- ('simple.test', 'degM'), 
- ('simple.test', 'sinM'), 
- ('simple.test', 'unixTimeM')]    
+params = [
+    ("simple.test", "shortWatchM"),
+    ("simple.test", "ushortWatchM"),
+    ("simple.test", "ushortWatchStringM"),
+    ("simple.test", "stringMonitorM"),
+    ("simple.test", "menuM"),
+    ("simple.test", "intM"),
+    ("simple.test", "longM"),
+    ("simple.test", "doubleM"),
+    ("simple.test", "intArrayM"),
+    ("simple.test", "degM"),
+    ("simple.test", "sinM"),
+    ("simple.test", "unixTimeM"),
+]
 
 
 req = Multirequest()
 responses = req.get(*params)
 
-r = [(param, responses[param], responses[(*param, "timestampSeconds")] + responses[(*param, "timestampNanoSeconds")] / 1e-9) for param in params if param in responses])
+# r = [(param, responses[param], responses[(*param, "timestampSeconds")] + responses[(*param, "timestampNanoSeconds")] / 1e-9) for param in params if param in responses])
 
-print(r)
+# print(r)
